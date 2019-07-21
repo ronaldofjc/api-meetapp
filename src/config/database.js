b1847361e12648edd8412bb1abbcfd12c275c9f5
Ronaldo <rfeitosa@ciandt.com>
2019-07-21 12:27:44 -0300

    Sequelize Configuration

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: null,
+  database: 'meetapp',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
